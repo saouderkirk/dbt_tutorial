c7ad317af1281051e9aa7437c5e77b62f97fe8ba
Simon O <saouderkirk@gmail.com>
2020-03-21 19:02:29 -0400

    Added intermediate tables & adjusted presentation area query

--- models/customers.sql
+++ models/customers.sql
@@ -1,19 +1,17 @@
 with customers as (
-    select
-        id as customer_id,
-        first_name,
-        last_name
-    from `dbt-tutorial`.jaffle_shop.customers
+
+	select * from {{ ref('stg_customers') }}
+
 ),
+
 orders as (
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-    from `dbt-tutorial`.jaffle_shop.orders
+
+	select * from {{ ref('stg_orders') }}
+
 ),
+
 customer_orders as (
+
     select
         customer_id,
         min(order_date) as first_order_date,
@@ -21,8 +19,11 @@ customer_orders as (
         count(order_id) as number_of_orders
     from orders
     group by 1
+
 ),
+
 final as (
+
     select
         customers.customer_id,
         customers.first_name,
@@ -32,5 +33,7 @@ final as (
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders
     from customers
     left join customer_orders using (customer_id)
+
 )
+
 select * from final
